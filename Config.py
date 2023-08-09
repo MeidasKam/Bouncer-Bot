b0440898bfd3a7c53896e7f5070841f1073013e4
Meidas <110493573+MeidasKam@users.noreply.github.com>
2023-08-09 04:00:51 +0100

    Updating Config File

--- Config.py
+++ Config.py
@@ -1,9 +1,32 @@
 MainBot = {
     "Token":""
 }
+Ranks = {
+    "Starter":0,
+    "Level_0":0,
+    "Level_1":0,
+    "Level_2":0,
+    "Level_3":0,
+    "Level_4":0,
+    "Level_5":0,
+    "Council":0
+}
+ServerBot = {
+    "Servers":[0],
+    "Admins":[0,0,0],
+    "Rules":["",
+             "",
+             "",
+             "",
+             "",
+             "",
+             "",
+             "",
+             "",
+             "",]
+}
 PublicBot = {
-    "Invite_Link":"",
-    "Support_Server_Link":"",
+    "Support_Server_Link":""
 }
 Economy = {
     "Currency_Emoji":"",
